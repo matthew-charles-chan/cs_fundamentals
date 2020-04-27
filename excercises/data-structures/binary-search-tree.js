3875c3dab2b1dd71f6a6ea8622ae74013dd705bf
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-04-27 11:15:52 -0700

    implement binary search tree

---
+++ excercises/data-structures/binary-search-tree.js
@@ -0,0 +1,43 @@
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  toObject() {
+    return this.root
+  }
+  add(value) {
+    if (this.root === null) {
+      // if no root, create root
+      this.root = new Node(value);
+      return;
+    }
+
+    let current = this.root;
+    while(true) {
+      if(current.value > value) {
+        if (current.left) {
+          current = current.left
+        }
+        else {
+          current.left = new Node(value)
+          return
+        }
+      }  
+        if (current.right) {
+          current = current.right
+        }
+        else {
+          current.right = new Node(value);
+          return
+        }
+    }
+  }
+}
+
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
